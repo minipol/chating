4ee01ad390e2abdf285dec849cad0354ae0631b1
svva9 <Den-ar9@yandex.ru>
2024-02-17 12:30:25 +0300

    1.добавление очистки поля ввода 2.добавление смиволов в чат 3.добавление алфавита

--- chat.py
+++ chat.py
@@ -21,15 +21,24 @@ def print_at(r, c, s):
 
 ch='asd\nzcv'
 sender=''
+alphavit = [chr(i) for i in range(97, 123)]
 
 while True:  # making a loop
     print_at(0,0,ch)
     print_at(20,0,'>'+sender)
     try:  # used try so that if user pressed other than the given key error will not be shown
-        if keyboard.is_pressed('q'):  # if key 'q' is pressed
-            sender+='q'
+        if keyboard.is_pressed('enter'):
+            ch+=('\n'+sender)
+            sender=''
+            os.system('cls')
             time.sleep(0.1)
-            # print('You Pressed A Key!')
-            # break  # finishing the loop
+        if keyboard.is_pressed('backspace'):
+            sender=sender[:-1]
+            os.system('cls')
+            time.sleep(0.1)
+        for bukva in alphavit:
+            if keyboard.is_pressed(bukva):  # if key 'q' is pressed
+                sender+=bukva
+                time.sleep(0.1)
     except:
         break  # if user pressed a key other than the given key the loop will break
