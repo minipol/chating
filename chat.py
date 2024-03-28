3581493c382d3b862c58ea742fcd5779bcd2470c
minipol <ninevoso@gmail.com>
2024-03-28 14:37:42 +0300

    v 0.5

--- chat.py
+++ chat.py
@@ -31,7 +31,7 @@ alphavit = [chr(i) for i in range(97, 123)]
 
 # Создание сокета и подключения к серверу
 sock = socket.socket()
-sock.connect(('192.168.1.14', 9090))
+sock.connect(('192.168.1.61', 9090))
 
 # Создание функции для вывода чата
 def out_chat():
@@ -45,14 +45,15 @@ def out_chat():
         # Вывод чата в координатах 0,0
         print_at(0,0,ch)
         # Вывод то что ввел пользователь в координатах 20,0
-        print_at(20,0,'>'+sender)
+        print_at(20,0,os.getlogin()+'> '+sender)
+        # print(os.getlogin())
 
         # Используется попытка, чтобы при нажатии пользователем другой клавиши, кроме данной, ошибка не отображалась
         try:
             # Если пользователь нажал enter то 
             if keyboard.is_pressed('enter'):
                 # Отправляем на сервер сообщение
-                send_server(sender+"\n")
+                send_server(os.getlogin() + ": " + sender+"\n")
                 # Очищаем то что ввел пользователь
                 sender=''
                 # Очищаем консоль
@@ -65,10 +66,19 @@ def out_chat():
                 sender=sender[:-1]
                 os.system('cls')
                 time.sleep(0.1)
+            if keyboard.is_pressed("space"):  # если нажата клавиша «q»
+                sender+=" "
+                os.system('cls')
+                time.sleep(0.1)
+            if keyboard.is_pressed(":"):  # если нажата клавиша «q»
+                sender+=":"
+                os.system('cls')
+                time.sleep(0.1)
             # Если пользователь нажал на букву из алфавита то она вставится в sender
             for bukva in alphavit:
                 if keyboard.is_pressed(bukva):  # если нажата клавиша «q»
                     sender+=bukva
+                    os.system('cls')
                     time.sleep(0.1)
         except:
             break  # если пользователь нажал клавишу, отличную от данной, цикл прервется
@@ -82,7 +92,8 @@ def recv_server():
     while True:
         # Получаем данные с сервера и добавляем в ch
         ch= sock.recv(1024).decode()
-        time.sleep(1)
+        sock.send("-".encode())
+        time.sleep(0.1)
         os.system('cls')
 
 # Отправка сервера сообщения
@@ -91,18 +102,26 @@ def send_server(msg):
 
     # while True:
     sock.send(msg.encode())
-    time.sleep(1)
+    # time.sleep(1)
     os.system('cls')
+
+# Отправка сервера сообщения
+def send_server2():
+    global sock
+
+    # while True:
+    #     sock.send("-".encode())
+    #     time.sleep(1)
 
 # Потоки
 t1 = threading.Thread(target=out_chat)
 t2 = threading.Thread(target=recv_server)
-# t3 = threading.Thread(target=send_server)
+t3 = threading.Thread(target=send_server2)
 
 t1.start()
 t2.start()
-# t3.start()
+t3.start()
 
 t1.join()
 t2.join()    
-# t3.join()    
+t3.join()    
--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 import socket #Для работы с сетью
 import threading #Для работы с потоками
 import select #Для: ?
+import time
 
 #Создается сокет и записывается в переменную
 sock = socket.socket()
 #"Создание сервера" и настройка
-sock.bind(('192.168.1.14', 9090))
+sock.bind(('192.168.1.61', 9090))
 #Запускаем сервер на прослушивание порта
 sock.listen()
 
@@ -21,23 +22,26 @@ def recv_clients():
     global index
     global message
     
-    while True:
-        for i in range(len(arr_conn)):
-            timeout=10
-            ready_socket,_,_=select.select([arr_conn[i][0]],[],[],None)
-            if ready_socket:
-                if arr_conn[i][0].fileno():
-                    try:
-                        message+=arr_conn[i][0].recv(1024).decode()
-                    except Exception:
-                        print(arr_conn[i][0])
-                        print("offline")
-                        arr_conn.pop(0)
-                        index-=1
-                else:
-                    print('bim')
-            else:
-                print('bam')
+    # while True:
+    #     for i in range(len(arr_conn)):
+    #         timeout=10
+    #         ready_socket,_,_=select.select([arr_conn[i][0]],[],[],None)
+    #         if ready_socket:
+    #             if arr_conn[i][0].fileno():
+    #                 try:
+    #                     message+=arr_conn[i][0].recv(1024).decode()
+    #                     if message[len(message) - 1] == "-":
+    #                         message=message[:-1]
+    #                     time.sleep(1)
+    #                 except Exception:
+    #                     print(arr_conn[i][0])
+    #                     print("offline")
+    #                     arr_conn.pop(0)
+    #                     index-=1
+    #             else:
+    #                 print('bim')
+    #         else:
+    #             print('bam')
 
 #Для того чтобы принимать подключение других пользователей
 def acept_klient():
@@ -61,6 +65,25 @@ def send_klients():
     global message
 
     while True:
+        for i in range(len(arr_conn)):
+            timeout=10
+            ready_socket,_,_=select.select([arr_conn[i][0]],[],[],None)
+            if ready_socket:
+                if arr_conn[i][0].fileno():
+                    try:
+                        message += arr_conn[i][0].recv(1024).decode().replace('-', '')
+                        # if tttt.find('-')==-1:
+                        #     message+=tttt
+                        # time.sleep(1)
+                    except Exception:
+                        print(arr_conn[i][0])
+                        print("offline")
+                        arr_conn.pop(0)
+                        index-=1
+                else:
+                    print('bim')
+            else:
+                print('bam')
         for i in arr_conn:
             i[0].send(message.encode())
 
