fc9bb275fa3f1197fbffc875e59c78dd29169f9a
svva9 <Den-ar9@yandex.ru>
2024-03-30 10:09:31 +0300

    финальное обновление (не точно)

--- chat.py
+++ chat.py
@@ -28,108 +28,82 @@ print('введите ник: ')
 nicname = input()
 os.system('cls')
 
-# Хранит весь чат
-ch='conecting\n'
-# Хранит то что ввел пользователь
-sender=''
-# Хранит английский алфавит
-alphavit = [chr(i) for i in range(97, 123)]
+ch='conecting\n'                            # Хранит весь чат
+sender=''                                   # Хранит то что ввел пользователь
+alphavit = [chr(i) for i in range(97, 123)] # Хранит английский алфавит
 
 # Создание сокета и подключения к серверу
 sock = socket.socket()
 sock.connect((ip_input, 9090))
 sock.send("-".encode())
 
-# Создание функции для вывода чата
-def out_chat():
+def out_chat(): # Создание функции для вывода чата
     global ch
     global sender
     global alphavit
     global sock
     global nicname
 
-    # Создание цикла
-    while True:
-        # Вывод чата в координатах 0,0
-        print_at(0,0,ch)
-        # Вывод то что ввел пользователь в координатах 20,0
-        print_at(20,0,nicname+'> '+sender)
-        # print(os.getlogin())
+    while True: # Создание цикла
+        print_at(0,0,ch)                    # Вывод чата в координатах 0,0
+        print_at(20,0,nicname+'> '+sender)  # Вывод то что ввел пользователь в координатах 20,0
 
-        # Используется попытка, чтобы при нажатии пользователем другой клавиши, кроме данной, ошибка не отображалась
-        try:
-            # Если пользователь нажал enter то 
-            if keyboard.is_pressed('enter'):
-                # Отправляем на сервер сообщение
-                send_server(nicname + ": " + sender+"\n")
-                # Очищаем то что ввел пользователь
-                sender=''
-                # Очищаем консоль
-                os.system('cls')
-                # Задержка
-                time.sleep(0.1)
-            # Если пользователь нажал backspace то
-            if keyboard.is_pressed('backspace'):
-                # Удаляем последний символ
-                sender=sender[:-1]
+        try:    # Используется попытка, чтобы при нажатии пользователем другой клавиши, кроме данной, ошибка не отображалась 
+            if keyboard.is_pressed('enter'):    # Если пользователь нажал enter то
+                send_server(nicname + ": " + sender+"\n")   # Отправляем на сервер сообщение
+                sender=''                                   # Очищаем то что ввел пользователь
+                os.system('cls')                            # Очищаем консоль
+                time.sleep(0.1)                             # Задержка
+
+            if keyboard.is_pressed('backspace'):    # Если пользователь нажал "backspace" то
+                sender=sender[:-1]  # Удаляем последний символ
                 os.system('cls')
                 time.sleep(0.1)
-            if keyboard.is_pressed("space"):  # если нажата клавиша «q»
+            if keyboard.is_pressed("space"):  # если пользователь нажал "space" то
                 sender+=" "
-                os.system('cls')
+                os.system('cls')            #обновляем консоль
                 time.sleep(0.1)
-            if keyboard.is_pressed(":"):  # если нажата клавиша «q»
+            if keyboard.is_pressed(":"):    #отбражение ":"
                 sender+=":"
                 os.system('cls')
                 time.sleep(0.1)
-            # Если пользователь нажал на букву из алфавита то она вставится в sender
-            for bukva in alphavit:
-                if keyboard.is_pressed(bukva):  # если нажата клавиша «q»
+            for bukva in alphavit:  # Если пользователь нажал на букву из алфавита то она вставится в "sender"
+                if keyboard.is_pressed(bukva):  # отображеине нажатых букв
                     sender+=bukva
                     os.system('cls')
                     time.sleep(0.1)
         except:
             break  # если пользователь нажал клавишу, отличную от данной, цикл прервется
 
-# Получения сообщения от сервера
-def recv_server():
+def recv_server():  # Получения сообщения от сервера
     global ch
     global sock
 
-# Бесконечный цикл
-    while True:
-        # Получаем данные с сервера и добавляем в ch
-        ch= sock.recv(1024).decode()
+    while True: # Бесконечный цикл
+        ch= sock.recv(1024).decode()     #Получаем данные с сервера и добавляем в ch
         sock.send("-".encode())
         time.sleep(0.1)
         os.system('cls')
 
-# Отправка сервера сообщения
-def send_server(msg):
+def send_server(msg):   # Отправка сервера сообщения
     global sock
-
-    # while True:
     sock.send(msg.encode())
-    # time.sleep(1)
     os.system('cls')
 
-# Отправка сервера сообщения
 def send_server2():
     global sock
-
-    # while True:
-    #     sock.send("-".encode())
-    #     time.sleep(1)
 
 # Потоки
 t1 = threading.Thread(target=out_chat)
 t2 = threading.Thread(target=recv_server)
 t3 = threading.Thread(target=send_server2)
 
+# запуск
 t1.start()
 t2.start()
 t3.start()
 
+# соеденение
 t1.join()
 t2.join()    
 t3.join()    
--- server.py
+++ server.py
@@ -1,14 +1,15 @@
-import socket #Для работы с сетью
-import threading #Для работы с потоками
-import select #Для: ?
+#импорт билиотек
+import socket 
+import threading 
+import select 
 import time
 
 print('введите ip: ')
 ip_input = input()
 
-sock = socket.socket()              #Создается сокет и записывается в переменную
-sock.bind((ip_input, 9090))         #"Создание сервера" и настройка
-sock.listen()                       #Запускаем сервер на прослушивание порта
+sock = socket.socket()      #Создается сокет и записывается в переменную
+sock.bind((ip_input, 9090)) #"Создание сервера" и настройка
+sock.listen()               #Запускаем сервер на прослушивание порта
 
 arr_conn = []   #Храним подключение пользователей (клиентов)
 index = 0       #Счетчик подключенных пользователей
@@ -19,61 +20,32 @@ def recv_clients():
     global arr_conn
     global index
     global message
-    
-    # while True:
-    #     for i in range(len(arr_conn)):
-    #         timeout=10
-    #         ready_socket,_,_=select.select([arr_conn[i][0]],[],[],None)
-    #         if ready_socket:
-    #             if arr_conn[i][0].fileno():
-    #                 try:
-    #                     message+=arr_conn[i][0].recv(1024).decode()
-    #                     if message[len(message) - 1] == "-":
-    #                         message=message[:-1]
-    #                     time.sleep(1)
-    #                 except Exception:
-    #                     print(arr_conn[i][0])
-    #                     print("offline")
-    #                     arr_conn.pop(0)
-    #                     index-=1
-    #             else:
-    #                 print('bim')
-    #         else:
-    #             print('bam')
 
-#Для того чтобы принимать подключение других пользователей
-def acept_klient():
+def acept_klient(): #Для того чтобы принимать подключение других пользователей
     global sock
     global arr_conn
     global index
 
-    #Для подключение многих пользователей 
-    while True:
-        #Добавление подключенного пользователя, подключение пользователя
-        arr_conn.append(sock.accept())
-        #Выводим в консоль подключеного пользователей
-        print ('connected:',arr_conn[index][1])
-        #Обновление счетчика
-        index+=1
+    while True: #Для подключение многих пользователей 
+        arr_conn.append(sock.accept())          #Добавление подключенного пользователя, подключение пользователя
+        print ('connected:',arr_conn[index][1]) #Выводим в консоль подключеного пользователей
+        index+=1        #Обновление счетчика
 
-def send_klients():
+def send_klients(): #Создание функции получения и отправки всем пользователям
     global sock
     global arr_conn
     global index
     global message
 
-    while True:
-        for i in range(len(arr_conn)):
+    while True: #Создание бесконечноого цикла
+        for i in range(len(arr_conn)):  #Создание цикла для пробегания по соединениям
             timeout=10
-            ready_socket,_,_=select.select([arr_conn[i][0]],[],[],None)
+            ready_socket,_,_=select.select([arr_conn[i][0]],[],[],None) #Для проверки отправил ли соединения сообщения
             if ready_socket:
-                if arr_conn[i][0].fileno():
+                if arr_conn[i][0].fileno(): #Проверка что соединение существует
                     try:
-                        message += arr_conn[i][0].recv(1024).decode().replace('-', '')
-                        # if tttt.find('-')==-1:
-                        #     message+=tttt
-                        # time.sleep(1)
-                    except Exception:
+                        message += arr_conn[i][0].recv(1024).decode().replace('-', '')  #Получение и сохранение сообщения от соединении
+                    except Exception:   #В случае ошибки убираем соединение, пишем в консоль что отключился и уменьшаем количество подключенных на 1
                         print(arr_conn[i][0])
                         print("offline")
                         arr_conn.pop(0)
@@ -82,18 +54,20 @@ def send_klients():
                     print('bim')
             else:
                 print('bam')
-        for i in arr_conn:
+        for i in arr_conn:  #Всем рассылаем сообщение
             i[0].send(message.encode())
 
+#Запускаем в потоки для функции
 t1 = threading.Thread(target=acept_klient)
 t2 = threading.Thread(target=send_klients)
 t3 = threading.Thread(target=recv_clients)
 
+# Запуск
 t1.start()
 t2.start()
 t3.start()
 
+#Соединение потоков
 t1.join()
 t2.join()     
 t3.join()     
-# conn.close()
